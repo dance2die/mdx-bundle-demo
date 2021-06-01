df392f803ae596bf0db948eecd4f597c8c805f7f
dance2die <sungmkim80@gmail.com>
2021-06-01 16:02:06 -0400

    Uses getSTaticPaths to generate static pages

--- pages/blog/[year].js
+++ pages/blog/[year].js
@@ -1,9 +1,42 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-function YearBlock() {
+import { readdir } from 'fs/promises';
+import path from 'path';
+
+async function getYears() {
+	const blogDirectory = path.join(process.cwd(), 'content', 'blog');
+	return await readdir(blogDirectory);
+}
+
+export async function getStaticPaths() {
+	const years = await getYears();
+	const paths = years.map((year) => ({ params: { year } }));
+	return { paths, fallback: false };
+}
+
+// export function getServerSideProps() {
+export async function getStaticProps() {
+	const blogDirectory = path.join(process.cwd(), 'content', 'blog');
+	const years = await getYears();
+
+	console.info({ years });
+
+	return {
+		props: { cwd: process.cwd(), dirname: __dirname, blogDirectory, years }
+	};
+}
+
+function YearBlock({ cwd, dirname, blogDirectory }) {
 	const router = useRouter();
 	const { year } = router.query;
+
+	useEffect(
+		() => {
+			console.info({ cwd, dirname, blogDirectory });
+		},
+		[ cwd, dirname, blogDirectory ]
+	);
 
 	return (
 		<main>
