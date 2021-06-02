129e4a0f5341a4c2495fbf0f26ba31e0883ff3c0
dance2die <sungmkim80@gmail.com>
2021-06-02 16:28:09 -0400

    able to show slug and frontmatters

--- pages/blog/[year].js
+++ pages/blog/[year].js
@@ -1,8 +1,33 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-import { readdir } from 'fs/promises';
+import { readdir, readFile } from 'fs/promises';
 import path from 'path';
+
+import { bundleMDX } from 'mdx-bundler';
+
+const getYearlyBlogRoot = (year) => path.join(process.cwd(), 'content', 'blog', year.toString());
+const getYearlyBlogDirectories = async (yearlyBlogRoot) => await readdir(yearlyBlogRoot);
+
+// Given a year, get all yearly posts' metadata
+async function getPostMetaListByYear(year) {
+	const yearlyBlogRoot = getYearlyBlogRoot(year);
+	const yearlyBlogDirectories = await getYearlyBlogDirectories(yearlyBlogRoot);
+	// return yearlyBlogDirectories;
+
+	const metadataListPromise = yearlyBlogDirectories.map(async (slug) => {
+		const blogPath = path.join(yearlyBlogRoot, slug, 'index.md');
+		const content = await readFile(blogPath, { encoding: 'utf8' });
+		const { frontmatter } = await bundleMDX(content.trim());
+
+		return { slug, frontmatter };
+	});
+
+	// const metadataList = Promise.all(metadataListPromise);
+	// return metadataList;
+
+	return metadataListPromise;
+}
 
 async function getYears() {
 	const blogDirectory = path.join(process.cwd(), 'content', 'blog');
@@ -18,12 +43,17 @@ export async function getStaticPaths() {
 // export function getServerSideProps() {
 export async function getStaticProps() {
 	const blogDirectory = path.join(process.cwd(), 'content', 'blog');
-	const years = await getYears();
+	// const years = await getYears();
 
-	console.info({ years });
+	const postsFor2020 = await getPostMetaListByYear(2021);
+	const resolved = await Promise.all(postsFor2020);
+
+	(async () => {
+		console.info({ postsFor2020: JSON.stringify(resolved, null, 2) });
+	})();
 
 	return {
-		props: { cwd: process.cwd(), dirname: __dirname, blogDirectory, years }
+		props: { cwd: process.cwd(), dirname: __dirname, blogDirectory }
 	};
 }
 
